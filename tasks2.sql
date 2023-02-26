ee59984a200dd03006bac0b473a708d6e8b4e1ab
nneennoo <126336963+nneennoo@users.noreply.github.com>
2023-02-26 16:42:21 +0300

    Add files via upload

---
+++ tasks2.sql
@@ -0,0 +1,9 @@
+SELECT *
+ FROM classicmodels.customers
+ where contactFirstName like 'je%' or 'le%';
+ SELECT *
+ FROM classicmodels.customers
+where contactFirstName like '%el' or  '%ie%';
+select *
+from classicmodels.customers
+ where contactFirstName like '%b%' ;
